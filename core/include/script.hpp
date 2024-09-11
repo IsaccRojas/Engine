e9c11f3fcf829c2dab18f267628ba5a0f2045dd1
IsaccRojas <irojas4@uic.edu>
2024-09-11 03:16:55 -0500

    Fix whitespace alignment

--- core/include/script.hpp
+++ core/include/script.hpp
@@ -23,69 +23,69 @@ class Servicer;
    expects _init(), _base(), and _kill() to be implemented by children. 
 */
 class Script {
-    friend Executor;
+   friend Executor;
 
-    // fields maintained by owning Executor
-    Executor *_executor;  
-    int _executor_id;
-    int _last_execqueue;
-    bool _removeonkill;
-    bool _initialized;
-    bool _killed;
-    bool _exec_enqueued;
-    bool _kill_enqueued; 
+   // fields maintained by owning Executor
+   Executor *_executor;  
+   int _executor_id;
+   int _last_execqueue;
+   bool _removeonkill;
+   bool _initialized;
+   bool _killed;
+   bool _exec_enqueued;
+   bool _kill_enqueued; 
 
-    // settable integer usable for identification
-    int _group;
+   // settable integer usable for identification
+   int _group;
 
-    // sets the Script up with an Executor
-    void _scriptRemove();
+   // sets the Script up with an Executor
+   void _scriptRemove();
 protected:
-    /* Functions to be overridden by children.
-       - _init() is called by runInit(). _runInit() is called on execution, only for the first time the Script is queued.
-       - _base() is called by runBase(). _runBase() is called on execution, each time the Script is queued.
-       - _kill() is called by runKill(). _runKill() is called on erasure.
-    */
-    virtual void _init() = 0;
-    virtual void _base() = 0;
-    virtual void _kill() = 0;
+   /* Functions to be overridden by children.
+      - _init() is called by runInit(). _runInit() is called on execution, only for the first time the Script is queued.
+      - _base() is called by runBase(). _runBase() is called on execution, each time the Script is queued.
+      - _kill() is called by runKill(). _runKill() is called on erasure.
+   */
+   virtual void _init() = 0;
+   virtual void _base() = 0;
+   virtual void _kill() = 0;
 
 public:
-    Script(Script &&other);
-    Script();
-    Script(const Script&) = delete;
-    virtual ~Script();
+   Script(Script &&other);
+   Script();
+   Script(const Script&) = delete;
+   virtual ~Script();
 
-    Script& operator=(Script &&other);
-    Script& operator=(const Script&) = delete;
+   Script& operator=(Script &&other);
+   Script& operator=(const Script&) = delete;
 
-   /* Functions wrapping the virtual versions of the same method, which are directly called by the Executor.
-       - runInit() is called on execution, only for the first time the Script is queued.
-       - runBase() is called on execution, each time the Script is queued.
-       - runKill() is called on erasure.
-    */
-    void runInit();
-    void runBase();
-    void runKill();
+/* Functions wrapping the virtual versions of the same method, which are directly called by the Executor.
+      - runInit() is called on execution, only for the first time the Script is queued.
+      - runBase() is called on execution, each time the Script is queued.
+      - runKill() is called on erasure.
+   */
+   void runInit();
+   void runBase();
+   void runKill();
 
-    /* Enqueues the Script for execution.
-       - queue - queue to enqueue into
-    */
-    void enqueueExec(unsigned queue);
+   /* Enqueues the Script for execution.
+      - queue - queue to enqueue into
+   */
+   void enqueueExec(unsigned queue);
 
-    /* Kills the Script. */
-    void enqueueKill();
+   /* Kills the Script. */
+   void enqueueKill();
 
-    /* Sets various internal flags used by Executors to control state. Can be set manually to manipulate
-       execution behavior.
-    */
-    
-    int getLastExecQueue();
-    bool getInitialized();
-    bool getKilled();
-    bool getExecEnqueued();
-    bool getKillEnqueued();
-    int getGroup();
+   /* Sets various internal flags used by Executors to control state. Can be set manually to manipulate
+      execution behavior.
+   */
+   
+   int getLastExecQueue();
+   bool getInitialized();
+   bool getKilled();
+   bool getExecEnqueued();
+   bool getKillEnqueued();
+   int getGroup();
 };
 
 // --------------------------------------------------------------------------------------------------------------------------
@@ -111,52 +111,52 @@ class GenericAllocator : public AllocatorInterface {
 };
 
 /* abstract class Receiver
-   Interface that is used to allow reception of a generic specified type when
-   implemented, via subscription to a Servicer of the same type.
+Interface that is used to allow reception of a generic specified type when
+implemented, via subscription to a Servicer of the same type.
 */
 template<class T>
 class Receiver {
-    friend Servicer<T>;
-    int _channel;
+   friend Servicer<T>;
+   int _channel;
 protected:
-    virtual void _receive(T *t) = 0;
-    Receiver() : _channel(-1) {}
+   virtual void _receive(T *t) = 0;
+   Receiver() : _channel(-1) {}
 public:
-    void setChannel(int channel) { _channel = channel; }
-    int getChannel() { return _channel; }
+   void setChannel(int channel) { _channel = channel; }
+   int getChannel() { return _channel; }
 };
 
 /* class Servicer
-   Implementation of AllocatorInterface that interprets the tag argument as a
-   "channel". Subscribed Receivers will have their _receive() method invoked
-   whenever instances of this class have their allocator invoked. Only Receivers
-   with a matching tag value will be passed the allocated instance of T.
+Implementation of AllocatorInterface that interprets the tag argument as a
+"channel". Subscribed Receivers will have their _receive() method invoked
+whenever instances of this class have their allocator invoked. Only Receivers
+with a matching tag value will be passed the allocated instance of T.
 */
 template<class T>
 class Servicer : public AllocatorInterface {
-    std::unordered_set<Receiver<T>*> _receivers;
-    
-    Base * _allocate(int tag) override {
-        // interpret tag as channel
+   std::unordered_set<Receiver<T>*> _receivers;
+   
+   Base * _allocate(int tag) override {
+      // interpret tag as channel
 
-        T *t = _allocateInstance();
+      T *t = _allocateInstance();
 
-        // if channel is non-negative, deliver instance
-        if (tag >= 0) {
+      // if channel is non-negative, deliver instance
+      if (tag >= 0) {
 
-            // if channel matches, deliver
-            for (const auto& receiver: _receivers)
-                if (receiver->_channel == tag)
-                    receiver->_receive(t);
-        }
-        return t;
-    }
+         // if channel matches, deliver
+         for (const auto& receiver: _receivers)
+               if (receiver->_channel == tag)
+                  receiver->_receive(t);
+      }
+      return t;
+   }
 protected:
-    virtual T *_allocateInstance() { return new T; }
+   virtual T *_allocateInstance() { return new T; }
 public:
-    void subscribe(Receiver<T> *receiver) {
-        _receivers.insert(receiver);
-    }
+   void subscribe(Receiver<T> *receiver) {
+      _receivers.insert(receiver);
+   }
 };
 
 // --------------------------------------------------------------------------------------------------------------------------
@@ -171,132 +171,132 @@ public:
 */
 class Executor {
 public:
-    // struct holding Script information mapped to a name
-    struct ScriptInfo {
-       int _group;
-       bool _removeonkill;
-       AllocatorInterface *_allocator;
-       std::function<void(unsigned)> _spawn_callback;
-       std::function<void(unsigned)> _remove_callback;
-    };
+   // struct holding Script information mapped to a name
+   struct ScriptInfo {
+      int _group;
+      bool _removeonkill;
+      AllocatorInterface *_allocator;
+      std::function<void(unsigned)> _spawn_callback;
+      std::function<void(unsigned)> _remove_callback;
+   };
 
-    // struct holding IDs and other flags belonging to the managed Script during its lifetime
-    struct ScriptValues {
-       const char *_manager_name;
-       int _group;
-    };
+   // struct holding IDs and other flags belonging to the managed Script during its lifetime
+   struct ScriptValues {
+      const char *_manager_name;
+      int _group;
+   };
 
-    // _valid flag is used to prevent instance from being spawned as a Script
-    struct ScriptEnqueue {
-       std::string _name;
-       int _execution_queue;
-       int _tag;
-    };
+   // _valid flag is used to prevent instance from being spawned as a Script
+   struct ScriptEnqueue {
+      std::string _name;
+      int _execution_queue;
+      int _tag;
+   };
 
 protected:
-    /* Script data structures */
-    // IDs to distribute to Scripts
-    SlotVec _ids;
+   /* Script data structures */
+   // IDs to distribute to Scripts
+   SlotVec _ids;
 
-    // vector of unique_ptrs of Scripts; allocated references are stored here so that they are
-    // guaranteed to be deleted at some point, even if this vector remains unused
-    std::vector<std::unique_ptr<Script>> _scripts;
+   // vector of unique_ptrs of Scripts; allocated references are stored here so that they are
+   // guaranteed to be deleted at some point, even if this vector remains unused
+   std::vector<std::unique_ptr<Script>> _scripts;
 
-    // internal variables for added script information and active scripts
-    std::unordered_map<std::string, ScriptInfo> _scriptinfos;
-    std::vector<ScriptValues> _scriptvalues;
-    std::queue<ScriptEnqueue> _scriptenqueues;
-    
-    // queues of IDs to be executed; swapped on execution
-    struct QueuePair {
+   // internal variables for added script information and active scripts
+   std::unordered_map<std::string, ScriptInfo> _scriptinfos;
+   std::vector<ScriptValues> _scriptvalues;
+   std::queue<ScriptEnqueue> _scriptenqueues;
+   
+   // queues of IDs to be executed; swapped on execution
+   struct QueuePair {
       std::queue<unsigned> _push_execqueue;
       std::queue<unsigned> _run_execqueue;
-    };
-    std::vector<QueuePair> _queuepairs;
-    
-    // queue of IDS to be erased
-    std::queue<unsigned> _push_killqueue;
-    std::queue<unsigned> _run_killqueue;
-    
-    // maximum number of active Scripts allowed and current count
-    unsigned _max_count;
-    unsigned _count;
+   };
+   std::vector<QueuePair> _queuepairs;
+   
+   // queue of IDS to be erased
+   std::queue<unsigned> _push_killqueue;
+   std::queue<unsigned> _run_killqueue;
+   
+   // maximum number of active Scripts allowed and current count
+   unsigned _max_count;
+   unsigned _count;
 
-    bool _initialized;
+   bool _initialized;
 
-    // initializes Script's Executor-related fields
-    void _setupScript(Script *script, unsigned executor_id, bool removeonkill, int group);
+   // initializes Script's Executor-related fields
+   void _setupScript(Script *script, unsigned executor_id, bool removeonkill, int group);
 
-    // spawns a Script using a name previously added to this manager, and returns its ID
-    unsigned _spawnScript(const char *script_name, int execution_queue, int tag);
+   // spawns a Script using a name previously added to this manager, and returns its ID
+   unsigned _spawnScript(const char *script_name, int execution_queue, int tag);
 public:
-    /* Calls init() with the provided arguments. */
-    Executor(unsigned max_count, unsigned queues);
-    Executor(Executor &&other);
-    Executor();
-    Executor(const Executor &other) = delete;
-    virtual ~Executor();
+   /* Calls init() with the provided arguments. */
+   Executor(unsigned max_count, unsigned queues);
+   Executor(Executor &&other);
+   Executor();
+   Executor(const Executor &other) = delete;
+   virtual ~Executor();
 
-    Executor &operator=(Executor &&other);
-    Executor &operator=(const Executor &other) = delete;
+   Executor &operator=(Executor &&other);
+   Executor &operator=(const Executor &other) = delete;
 
-    void init(unsigned max_count, unsigned queues);
-    void uninit();
+   void init(unsigned max_count, unsigned queues);
+   void uninit();
 
-    /* Returns a pointer to a Script instance in the environment corresponding to the provided ID. Returns nullptr if the
-       ID does not exist in the environment.
-    */
-    Script* get(unsigned id);
-    /* Removes the provided ID from the system by making its ID available for writing. Note that it is undefined 
-       behavior to use the passed ID after calling this.
-    */
-    void remove(unsigned id);
+   /* Returns a pointer to a Script instance in the environment corresponding to the provided ID. Returns nullptr if the
+      ID does not exist in the environment.
+   */
+   Script* get(unsigned id);
+   /* Removes the provided ID from the system by making its ID available for writing. Note that it is undefined 
+      behavior to use the passed ID after calling this.
+   */
+   void remove(unsigned id);
 
-    /* Adds an Script allocator with initialization information to this manager, allowing its given
-       name to be used for future spawns.
-       - allocator - Reference to instance of class implementing AllocatorInterface.
-       - name - name to associate with the allocator
-       - group - value to associate with all instances of this Script
-       - removeonkill - removes this Script from this manager when it is killed
-       - spawn_callback - function callback to call after Script has been spawned and setup
-       - remove_callback - function callback to call before Script has been removed
-    */
-    void add(AllocatorInterface *allocator, const char *name, int group, bool removeonkill, std::function<void(unsigned)> spawn_callback, std::function<void(unsigned)>  remove_callback);
+   /* Adds an Script allocator with initialization information to this manager, allowing its given
+      name to be used for future spawns.
+      - allocator - Reference to instance of class implementing AllocatorInterface.
+      - name - name to associate with the allocator
+      - group - value to associate with all instances of this Script
+      - removeonkill - removes this Script from this manager when it is killed
+      - spawn_callback - function callback to call after Script has been spawned and setup
+      - remove_callback - function callback to call before Script has been removed
+   */
+   void add(AllocatorInterface *allocator, const char *name, int group, bool removeonkill, std::function<void(unsigned)> spawn_callback, std::function<void(unsigned)>  remove_callback);
 
-    /* Enqueues a Script to be spawned when calling runSpawnQueue(). */
-    virtual void enqueueSpawn(const char *script_name, int execution_queue, int tag);
-    /* Enqueues a Script instance corresponding to the ID provided to be executed when runExecQueue() is called. */
-    void enqueueExec(unsigned id, unsigned queue);
-    /* Enqueues a Script instance corresponding to the ID provided to be killed when runKillQueue() is called. */
-    void enqueueKill(unsigned id);
+   /* Enqueues a Script to be spawned when calling runSpawnQueue(). */
+   virtual void enqueueSpawn(const char *script_name, int execution_queue, int tag);
+   /* Enqueues a Script instance corresponding to the ID provided to be executed when runExecQueue() is called. */
+   void enqueueExec(unsigned id, unsigned queue);
+   /* Enqueues a Script instance corresponding to the ID provided to be killed when runKillQueue() is called. */
+   void enqueueKill(unsigned id);
 
-    /* Executes all currently enqueued Scripts in the specified queue, and dequeues them. This will call the 
-       (init() method if it has not yet been called, and the) base() method on every active Script.
-    */
-    void runExecQueue(unsigned queue);
-    /* Calls the kill() method on all erasure-queued Scripts if it has not been called yet. */
-    void runKillQueue();
-    /* Spawns all Scripts (or sub classes) queued for spawning with spawnScriptEnqueue(). */
-    virtual std::vector<unsigned> runSpawnQueue();
+   /* Executes all currently enqueued Scripts in the specified queue, and dequeues them. This will call the 
+      (init() method if it has not yet been called, and the) base() method on every active Script.
+   */
+   void runExecQueue(unsigned queue);
+   /* Calls the kill() method on all erasure-queued Scripts if it has not been called yet. */
+   void runKillQueue();
+   /* Spawns all Scripts (or sub classes) queued for spawning with spawnScriptEnqueue(). */
+   virtual std::vector<unsigned> runSpawnQueue();
 
-    /* Returns true if the provided ID is active. */
-    bool hasID(unsigned id);
-    /* Returns true if the provided Script name has been previously added to this manager. */
-    bool hasAdded(const char *script_name);
-    /* Returns a vector of IDs of all active Scripts with the corresponding group. */
-    std::vector<unsigned> getAllByGroup(int group);
-    /* Returns the internal name corresponding to the provided ID, if it exists. */
-    std::string getName(unsigned id);
-    /* Returns whether this instance has been initialized or not. */
-    bool getInitialized();
-    /* Returns the number of active scripts in the manager. */
-    unsigned getCount();
-    /* Returns the maximum number of active scripts allowed in the manager. */
-    unsigned getMaxCount();
-    /* Gets the maximum generated ID during this manager's lifetime. */
-    int getMaxID();
-    /* Returns number of execution queues in this instance. */
-    int getQueueCount();
+   /* Returns true if the provided ID is active. */
+   bool hasID(unsigned id);
+   /* Returns true if the provided Script name has been previously added to this manager. */
+   bool hasAdded(const char *script_name);
+   /* Returns a vector of IDs of all active Scripts with the corresponding group. */
+   std::vector<unsigned> getAllByGroup(int group);
+   /* Returns the internal name corresponding to the provided ID, if it exists. */
+   std::string getName(unsigned id);
+   /* Returns whether this instance has been initialized or not. */
+   bool getInitialized();
+   /* Returns the number of active scripts in the manager. */
+   unsigned getCount();
+   /* Returns the maximum number of active scripts allowed in the manager. */
+   unsigned getMaxCount();
+   /* Gets the maximum generated ID during this manager's lifetime. */
+   int getMaxID();
+   /* Returns number of execution queues in this instance. */
+   int getQueueCount();
 };
 
 #endif
