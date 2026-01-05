1ad0ae7a29bfe10859736f5f868e8aa4605cdb52
IsaccRojas <irojas4@uic.edu>
2026-01-04 21:51:33 -0600

    Rename Script base methods to exec methods, update example

--- examples/other/src/implementations.cpp
+++ examples/other/src/implementations.cpp
@@ -17,17 +17,17 @@ void ES_Player::_execEntity() {
     if (_input_state->get_d())
         pos += glm::vec3(speed, 0.0f, 0.0f);
 
-    entity().quads()[0]->bv_pos.v = pos;
-    entity().boxes()[0]->transform.pos = pos;
-
     if (_input_state->get_space())
         enqueueKill();
-    
     enqueueExec(0);
 }
 
 void ES_Player::_killEntity() {}
 
-void ES_Player::_updateEntity() {}
+void ES_Player::_updateEntity() {
+    glm::vec3 &pos = entity().attributes3f()["pos"];
+    entity().quads()[0]->bv_pos.v = pos;
+    entity().boxes()[0]->transform.pos = pos;
+}
 
 ES_Player::ES_Player(GLFWInput *input_state) : EntityScript(), _input_state(input_state) {}
